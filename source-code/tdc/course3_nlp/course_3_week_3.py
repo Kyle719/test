fbcb3e7337731ad056213c38da51076c2be6cc89
Kyle719 <64996393+Kyle719@users.noreply.github.com>
2020-06-16 23:10:00 +0900

    Add files via upload

---
+++ source-code/tdc/course3_nlp/course_3_week_3.py
@@ -0,0 +1,19 @@
+https://github.com/lmoroney/dlaicourse/tree/master/TensorFlow%20In%20Practice/Course%203%20-%20NLP
+
+
+1a
+Single Layer LSTM
+1b
+Multiple Layer LSTM
+1c
+Multiple Layer GRU
+
+
+2
+embedding, bidirection, dense
+2c
+embedding, conv1d, globalmaxpooling, dense
+2d
+2, 2c 합쳐놓음
+
+
